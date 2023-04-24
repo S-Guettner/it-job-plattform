2a3afef9d96ce8abff277b6080dd44dafad0076a
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-04-24 15:49:35 +0200

    feat(backend): added user schema

--- backend/user-schema.js
+++ backend/user-schema.js
@@ -1,24 +1,25 @@
 import mongoose from 'mongoose'
 
 
-const userData = mongoose.Schema(
+const userSchema = mongoose.Schema(
     {
         userEmail:{
             type:String,
             require:[true, "E-mail adress is required"],
             unique: true,
             length:1,
+            lowercase:true
         },
 
 
         userPassword: {
             type:String,
             require:[true, "Password is required"],
-            length:6
+            minLength:6
         }
     }
 )
 
-const user = mongoose.model('userData',userData )
+const user = mongoose.model('userSchema',userSchema )
 
 export default user
