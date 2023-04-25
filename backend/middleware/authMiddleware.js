28d4fefb31c16c23608890a7c33d1c998ddd36d3
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-04-25 14:21:26 +0200

    refactor: imported validation to index.js as middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,8 +1,20 @@
 import { createHmac } from 'crypto'
+import {check, validationResult } from 'express-validator'
 
-
+// password encryption
 export const encryptPassword = (req,_,next) => {
     const hmac = createHmac('sha512', req.body.userPassword)
     req.body.userPassword = hmac.digest('hex')
     next()
 }
+
+//e-mail validation
+export const validateUserEmail = check('userEmail')
+    .isEmail()
+    .withMessage('Invalid email address')
+
+//password -validation
+export const validateUserPassword = check('userPassword')
+    .isLength({ min: 8 }).withMessage('Password must be at least 8 characters long')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+{};:<,.>]).+$/)
+    .withMessage('Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character')
