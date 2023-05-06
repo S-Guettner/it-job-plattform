e8d3f6fb155905b69e4e2be1157dc669a87b1ec0
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-05-07 00:37:54 +0200

    feat: added full registration form data to model

--- backend/schema/userCompaniesSchema.js
+++ backend/schema/userCompaniesSchema.js
@@ -11,11 +11,53 @@ const userCompany = mongoose.Schema(
             lowercase:true
         },
 
-
         userPassword: {
             type:String,
             require:[true, "Password is required"],
-            minLength:6
+            minLength:8
+        },
+
+        firstName:{
+            type:String,
+            require:[true, "First Name is required"],
+            length:1
+        },
+
+        lastName:{
+            type:String,
+            require:[true, "Last Name is required"],
+            length:1
+        },
+
+        companyName:{
+            type:String,
+            require:[true, "Company Name is required"],
+            length:1
+        },
+
+        telephoneNumber:{
+            type:String,
+            unique: true,
+            require:[true, "Number is required"],
+            length:1
+        },
+
+        country:{
+            type:String,
+            require:[true, "Country is required"],
+            length:1
+        },
+
+        city:{
+            type:String,
+            require:[true, "City is required"],
+            length:1
+        },
+
+        zipCode:{
+            type:String,
+            require:[true, "Company Name is required"],
+            length:1
         }
     }
 )
